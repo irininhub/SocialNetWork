6e25cf4d359f6b510ab16ce723e3a01efec91bb8
Irina <irina.potrebko7@gmail.com>
2020-05-19 15:24:24 +0300

    fixed some bugs

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -3,7 +3,7 @@ import s from './Music.module.css';
 
 
 const Music = () => {
-    return <div>
+    return <div className={s.musicPage}>
       <div className={s.text}>  Music of my </div> <div className={s.heart}></div>
     </div>
 }
