af53522ceadcfa8ce59399878e99b1f7f0807764
Irina <irina.potrebko7@gmail.com>
2020-05-20 19:04:25 +0300

    Updates

---
+++ precache-manifest.6307c2a2f4d99037f85fea26362966db.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ab4d3a5044c35c9d11ad47ac8fa164",
+    "url": "/SocialNetWork/index.html"
+  },
+  {
+    "revision": "aa33b1b3be4623538424",
+    "url": "/SocialNetWork/static/css/3.8e1af24d.chunk.css"
+  },
+  {
+    "revision": "50ac53fcd54ffc4e9a1a",
+    "url": "/SocialNetWork/static/css/4.8c852f7e.chunk.css"
+  },
+  {
+    "revision": "9258f398ff00e6d166c0",
+    "url": "/SocialNetWork/static/css/main.81f5ebdb.chunk.css"
+  },
+  {
+    "revision": "dd0f0f14e22bae5be289",
+    "url": "/SocialNetWork/static/js/2.e4a5eb33.chunk.js"
+  },
+  {
+    "revision": "058c14a89d808fe8a38664b65e903dd7",
+    "url": "/SocialNetWork/static/js/2.e4a5eb33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa33b1b3be4623538424",
+    "url": "/SocialNetWork/static/js/3.9e9a2d09.chunk.js"
+  },
+  {
+    "revision": "50ac53fcd54ffc4e9a1a",
+    "url": "/SocialNetWork/static/js/4.62dcefb1.chunk.js"
+  },
+  {
+    "revision": "9258f398ff00e6d166c0",
+    "url": "/SocialNetWork/static/js/main.2f18c89e.chunk.js"
+  },
+  {
+    "revision": "b0c754dd35fdcaed51b7",
+    "url": "/SocialNetWork/static/js/runtime-main.f57a402a.js"
+  },
+  {
+    "revision": "4b2742fa241fb721dfdc1171213e0a14",
+    "url": "/SocialNetWork/static/media/204-2040760_contact-starwars-user-default-yoda-comments-users-icon.4b2742fa.png"
+  },
+  {
+    "revision": "b6a3ac27b2fba53d5474649fb7a38237",
+    "url": "/SocialNetWork/static/media/preloader.b6a3ac27.svg"
+  },
+  {
+    "revision": "a03d99f7a9a695d2dac59eeea7a5ff8b",
+    "url": "/SocialNetWork/static/media/users-selector.a03d99f7.bin"
+  }
+]);
